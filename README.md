6c6b353449cea8a81dfd8315f23449057f804bd3
MShumaker <ms106718@ohio.edu>
2023-02-20 20:22:29 -0500

    Basics set up of README and map.cc

---
+++ README.md
@@ -0,0 +1,7 @@
+# Maze Solver
+## CS 3560
+Chase Deweese
+Norman Lee
+Nick Andujar
+Tyler Hixon
+Max Shumaker
---
+++ map.cc
@@ -0,0 +1,18 @@
+/********
+ * Team: Maze Solver
+ * map.cc: Primary code for maze solver
+ * Class: CS 3560
+********/
+
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[]){
+    
+    
+    
+    
+    return 0;
+}
